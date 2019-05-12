8636ea90c5fb0407396b3c51fc97cc79c7cfc9f8
student9 <student9@pcoip.ki.agh.edu.pl>
2019-05-12 11:48:08 +0200

    Zajecia 12.05

--- src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
+++ src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
@@ -5,8 +5,10 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.util.Collection;
 
@@ -15,13 +17,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 import com.company.enroller.persistence.ParticipantService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ParticipantRestController.class)
@@ -47,6 +52,63 @@ public class ParticipantRestControllerTest {
 
 		mvc.perform(get("/participants").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].login", is(participant.getLogin())));
+	}
+
+	@Test
+	public void getParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+
+		mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(new ObjectMapper().writeValueAsString(participant)));
+	}
+
+	@Test
+	public void postParticipant() throws Exception {
+		Participant Participant = new Participant();
+		Participant.setLogin("testlogin");
+		Participant.setPassword("testpassword");
+
+		String partcipantStr = new ObjectMapper().writeValueAsString(Participant);
+
+		mvc.perform(MockMvcRequestBuilders.post("/participants").content(partcipantStr)
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andExpect(content().string(new ObjectMapper().writeValueAsString(Participant)));
+	}
+
+	@Test
+	public void addExistingParticipant() throws Exception {
+
+		Participant existingParticipant = new Participant();
+		existingParticipant.setLogin("testlogin");
+		existingParticipant.setPassword("testpassword");
+		given(participantService.findByLogin(existingParticipant.getLogin())).willReturn(existingParticipant);
+
+		String existingPartcipantStr = new ObjectMapper().writeValueAsString(existingParticipant);
+
+		mvc.perform(MockMvcRequestBuilders.post("/participants").content(existingPartcipantStr)
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isConflict());
+	}
+	
+	@Test
+	public void deleteParticipant() throws Exception {
+
+		Participant ParticipantForDeletion = new Participant();
+		ParticipantForDeletion.setLogin("testlogin");
+		ParticipantForDeletion.setPassword("testpassword");
+		given(participantService.findByLogin("testlogin")).willReturn(ParticipantForDeletion);
+		given(participantService.findByLogin("testlogin")).willReturn(ParticipantForDeletion);
+
+		String existingPartcipantStr = new ObjectMapper().writeValueAsString(ParticipantForDeletion);
+
+		mvc.perform(MockMvcRequestBuilders.post("/participants").content(existingPartcipantStr)
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNoContent());
 	}
 
 }
